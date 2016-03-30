0ca9045265579a44c128731a96d98a57106e9a61
usametov <usametov@gmail.com>
2016-03-30 21:35:37 +0600

    add web

---
+++ Web/Model/TagBundleRequest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Web.Model
+{
+    public class TagBundleRequest
+    {
+        public string[] TagBundle { get; set; }
+        public string[] ExcludeList { get; set; }        
+        public string BundleName { get; set; }
+        public int Threshold { get; set; }
+    }
+}
