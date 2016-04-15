5afe518835972d488ca48489151e4eac0b17e2c3
usametov <usametov@gmail.com>
2016-04-15 13:17:50 +0600

    add tag repo

---
+++ BookmarkRepository/TagRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Bookmarks5.Common;
+
+namespace BookmarkRepository
+{
+    public class TagRepository : ITagRepository
+    {
+        public void CreateTagBundle(string bundleName, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetExcludeList(string bundleName, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetMostFrequentTags(string bundleName, string bookmarksCollectionName, int threshold)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetTagAssociations(string bundleName, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetTagBundle(string bundleName, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetTagBundles(string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveExcludeList(string tagBundleName, IEnumerable<string> excludeList, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTagBundleList(string tagBundleName, IEnumerable<string> topTags, string bookmarksCollectionName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
