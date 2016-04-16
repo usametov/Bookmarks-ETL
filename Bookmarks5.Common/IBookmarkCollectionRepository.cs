1b728951d5e6bdf59a824d260a9d044a88f485be
usametov <usametov@gmail.com>
2016-04-16 11:30:00 +0600

    add bookmarksCollection repository interface

---
+++ Bookmarks5.Common/IBookmarkCollectionRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace Bookmarks5.Common
+{
+    public interface IBookmarkCollectionRepository
+    {
+        IEnumerable<string> GetBookmarkCollections();
+
+        void SetDefaultBookmarkCollection(string bookmarkCollection);
+    }
+}
