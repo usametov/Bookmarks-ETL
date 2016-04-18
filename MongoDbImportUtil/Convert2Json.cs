a3104f4b05bffadcde377a28eae2197e8fd58aa2
usametov <usametov@gmail.com>
2016-04-18 11:40:01 +0600

    add mongo import json

---
+++ MongoDbImportUtil/Convert2Json.cs
@@ -0,0 +1,22 @@
+﻿
+using Bookmarks.Common;
+using Newtonsoft.Json;
+//using System.IO;
+
+namespace MongoDbImportUtil
+{
+    public class Bookmarks2Json
+    {
+        public Bookmarks2Json(IBookmarkParser pars) {
+            Parser = pars;
+        }
+
+        public IBookmarkParser Parser { get; set; }
+
+        public string Write(string bookmarksFile, string outputPath) {
+            
+            var bookmarks = Parser.ParseBookmarks(bookmarksFile);
+            return JsonConvert.SerializeObject(bookmarks);
+        }
+    }
+}
