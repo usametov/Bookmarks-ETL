3c35e9ab47008c21fdb8d8e2ea764092ff207830
usametov <usametov@gmail.com>
2016-04-21 21:29:12 +0600

    add tag counts etl

---
+++ MongoDbImportUtil/etl-loadTagCounts.js
@@ -0,0 +1,12 @@
+function loadTagCounts(){
+	db.bookmarks.aggregate(
+		{
+			$project:
+			{"_id":0, "Tags":1}
+		},
+		{ $unwind: "$Tags" },
+		{$group:{_id:"$Tags", count:{$sum:1}}},
+		{$project:{_id:0, Tag:"$_id",count:1}}, 
+		{$sort:{count:-1}},
+		{$out:"tagCounts"});
+}
