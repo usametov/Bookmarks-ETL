2d1ddb26f17b41b46e4f97fbb4470bdaa24fa404
usametov <usametov@gmail.com>
2016-04-25 13:05:06 +0600

    implement and test add/update tagBundles

---
+++ Common/TagBundle.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Bookmarks.Common
+{    
+    public class TagBundle 
+    {
+        public string[] Tags { get; set; }
+        public string[] ExcludeTags { get; set; }
+        public string Name { get; set; }        
+    }
+}
