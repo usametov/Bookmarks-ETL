cabef475b76c11e708304ca9ac97b1ba22fd0293
usametov <usametov@gmail.com>
2016-09-15 19:03:14 +0600

    added bookmark class, added methods to get bookmarks

---
+++ Common/Bookmark.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Bookmarks.Common
+{
+    public class Bookmark : IBookmark
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public bool IsPrivate { get; set; }
+
+        public DateTime AddDate
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public string LinkText
+        {
+            get;
+            set;
+        }
+
+        public string LinkUrl
+        {
+            get;
+            set;
+        }
+
+        public List<string> Tags
+        {
+            get;
+            set;
+        }
+    }
+}
