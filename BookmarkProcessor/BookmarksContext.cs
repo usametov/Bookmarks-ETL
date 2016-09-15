b3bc280fdb18dd546ccf6d4b44037271c1627860
usametov <usametov@gmail.com>
2016-09-15 19:03:14 +0600

    added bookmark class, added methods to get bookmarks

--- BookmarkProcessor/BookmarksContext.cs
+++ BookmarkProcessor/BookmarksContext.cs
@@ -9,17 +9,20 @@ using System.Linq;
 
 namespace BookmarkProcessor
 {
-    public class MongoProcessor
+    public class BookmarksContext
     {
         public const string DEFAULT_BOOKMARKS_COLLECTION = "bookmarks";
         private const string DEFAULT_BOOKMARK_DB = "astanova-bookmarks";
         private const int TAG_COUNTS_PAGE_SIZE = 1000;
+        private const string TAG_BUNDLES_COLLECTION = "tagBundles";
 
         public string BookmarksCollection { get; set; }
+
+        // This is ok... Normally, we put into an IoC container.
         IMongoClient _client;
         IMongoDatabase _database;
 
-        public MongoProcessor(string connectionString, string bookmarksCollection = DEFAULT_BOOKMARKS_COLLECTION)
+        public BookmarksContext(string connectionString, string bookmarksCollection = DEFAULT_BOOKMARKS_COLLECTION)
         {
 
             ConnectionString = connectionString;
@@ -58,7 +61,7 @@ namespace BookmarkProcessor
         }
 
         /// <summary>
-        /// TODO: should this be cached?
+        /// Calculate term counts
         /// </summary>
         /// <returns></returns>
         public IEnumerable<TagCount> CalculateTermCounts()
@@ -166,14 +169,14 @@ namespace BookmarkProcessor
 
         public void CreateTagBundle(TagBundle tagBundle) {
 
-            var tagBundles = _database.GetCollection<TagBundle>("tagBundles");
+            var tagBundles = _database.GetCollection<TagBundle>(TAG_BUNDLES_COLLECTION);
 
             tagBundles?.InsertOne(tagBundle);
         }
 
         public void UpdateTagBundle(TagBundle tagBundle){
 
-            var tagBundles = _database.GetCollection<TagBundle>("tagBundles");
+            var tagBundles = _database.GetCollection<TagBundle>(TAG_BUNDLES_COLLECTION);
             var builder = Builders<TagBundle>.Filter;
             var filter = builder.Eq(t => t.Name, tagBundle.Name);
             var update = Builders<TagBundle>.Update
@@ -192,7 +195,7 @@ namespace BookmarkProcessor
         public IEnumerable<TagBundle> GetTagBundles(string name)
         {
             IEnumerable<TagBundle> result = null;
-            var tagBundles = _database.GetCollection<TagBundle>("tagBundles");
+            var tagBundles = _database.GetCollection<TagBundle>(TAG_BUNDLES_COLLECTION);
             
             if (string.IsNullOrEmpty(name)){
                 result = tagBundles.Find(new BsonDocument()).ToList();
@@ -211,10 +214,10 @@ namespace BookmarkProcessor
             if (string.IsNullOrEmpty(objId))
                 throw new ArgumentNullException("objId");
                         
-            var tagBundles = _database.GetCollection<TagBundle>("tagBundles");            
+            var tagBundles = _database.GetCollection<TagBundle>(TAG_BUNDLES_COLLECTION);            
                         
             var builder = Builders<TagBundle>.Filter;
-            var filter = builder.Eq(t => t.Id, new ObjectId(objId));
+            var filter = builder.Eq(t => t.Id, objId);
 
             var resultTask = tagBundles.Find(filter).FirstAsync();            
 
@@ -235,6 +238,22 @@ namespace BookmarkProcessor
                                         .OrderByDescending(tc => tc.Count).ToList();
 
             return filteredTags;
+        }
+
+        public IEnumerable<IBookmark> GetBookmarksByTagBundle(string tagBundleName) {
+
+            //get tag bundle by name
+            var tagBundle = GetTagBundles(tagBundleName).FirstOrDefault();
+
+            if (tagBundle == null)
+                throw new ApplicationException("tagBundle not found");
+
+            var bookmarks = _database.GetCollection<Bookmark>(BookmarksCollection);
+            //should be in tagBundle.Tags
+            //HACK!
+            var filterDef = string.Format("{{ 'Tags': {{$elemMatch: {{$in: ['{0}'] }} }} }}", string.Join("','", tagBundle.Tags));
+            
+            return bookmarks.Find(filterDef).ToList();            
         }
     }
 }
--- Common/TagBundle.cs
+++ Common/TagBundle.cs
@@ -1,11 +1,12 @@
-﻿
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Bookmarks.Common
 {    
     public class TagBundle 
     {
-        public ObjectId Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
         public string[] Tags { get; set; }
         public string[] ExcludeTags { get; set; }
         public string Name { get; set; }        
