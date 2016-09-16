6f99128850ee0843df3bdc44289d47aa389263c7
usametov <usametov@gmail.com>
2016-09-16 12:20:58 +0600

    add user class

---
+++ Common/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Bookmarks.Common
+{
+    public class User
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
