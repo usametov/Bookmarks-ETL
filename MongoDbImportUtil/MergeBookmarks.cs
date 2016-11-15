58fa92cff368f666697aea3d8b7d5d0996b8c6d9
usametov <usametov@gmail.com>
2016-11-15 18:41:40 +0600

    add entry checks

--- MongoDbImportUtil/MergeBookmarks.cs
+++ MongoDbImportUtil/MergeBookmarks.cs
@@ -1,5 +1,6 @@
 ﻿using Bookmarks.Common;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +10,10 @@ namespace MongoDbImportUtil
     {
         public static List<Bookmark> Merge(string bookmarksFile1, string bookmarksFile2)
         {
+            if (string.IsNullOrEmpty(bookmarksFile1)
+             || string.IsNullOrEmpty(bookmarksFile2))
+                throw new ArgumentException("empty path provided");
+
             var bookmarks1 = JsonConvert.DeserializeObject<List<Bookmark>>(bookmarksFile1);
             var bookmarks2 = JsonConvert.DeserializeObject<List<Bookmark>>(bookmarksFile2);            
             //union all
@@ -31,6 +36,9 @@ namespace MongoDbImportUtil
 
         private static bool Equals(Bookmark b1, Bookmark b2)
         {
+            if (b1 == null || b2 == null)
+                throw new ArgumentException("b1 or b2 is null");
+
             var tag1 = new HashSet<string>(b1.Tags.Distinct());
             var tag2 = new HashSet<string>(b2.Tags.Distinct());
                         
