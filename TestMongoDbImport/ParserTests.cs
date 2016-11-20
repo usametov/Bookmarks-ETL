b0b4d395bf0a7a8ca2f701632ea4de2f907e40db
usametov <usametov@gmail.com>
2016-11-20 22:07:04 +0600

    add gitmarks test

--- TestMongoDbImport/ParserTests.cs
+++ TestMongoDbImport/ParserTests.cs
@@ -14,11 +14,27 @@ namespace TestMongoDbImport
             Assert.IsNotEmpty(bookmarks);
         }
 
-        [TestCase(@"C:\code\csharp6\Tagging-Util\storage\bibsonomy.xml")]
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\bibsonomy.xml")]
         public void TestBibsonomyParser(string bookmarksFile)
         {
             var parser = new BibsonomyParser.Parser();
             var bookmarks = parser.ParseBookmarks(bookmarksFile);
+            Assert.IsNotEmpty(bookmarks);
+        }
+
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\pins.txt")]
+        public void TestPinterestParser(string bookmarksFile)
+        {
+            var parser = new PinterestParser.Parser();
+            var bookmarks = parser.ParseBookmarks(bookmarksFile);
+            Assert.IsNotEmpty(bookmarks);
+        }
+
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\gitmarks\")]
+        public void TestGitmarksParser(string bookmarksRepo)
+        {
+            var parser = new GitmarksParser.Parser();
+            var bookmarks = parser.ParseBookmarks(bookmarksRepo);
             Assert.IsNotEmpty(bookmarks);
         }
     }
