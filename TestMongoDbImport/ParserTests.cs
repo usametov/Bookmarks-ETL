96248a3afcd2f95f43a16794880d050c4dbedaab
usametov <usametov@gmail.com>
2016-11-20 21:00:52 +0600

    add test project

---
+++ TestMongoDbImport/ParserTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestMongoDbImport
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\delicious-2016-03-16.html")]        
+        public void TestDeliciousParser(string bookmarksFile)
+        {
+            var parser = new DeliciousParser.Parser();
+            var bookmarks = parser.ParseBookmarks(bookmarksFile);
+            Assert.IsNotEmpty(bookmarks);
+        }
+
+        [TestCase(@"C:\code\csharp6\Tagging-Util\storage\bibsonomy.xml")]
+        public void TestBibsonomyParser(string bookmarksFile)
+        {
+            var parser = new BibsonomyParser.Parser();
+            var bookmarks = parser.ParseBookmarks(bookmarksFile);
+            Assert.IsNotEmpty(bookmarks);
+        }
+    }
+}
