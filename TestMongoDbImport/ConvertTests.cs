18d44a4a547775e360cf275e7921b2bc0ad220ba
usametov <usametov@gmail.com>
2016-11-21 14:10:14 +0600

    add test cases

--- TestMongoDbImport/ConvertTests.cs
+++ TestMongoDbImport/ConvertTests.cs
@@ -9,7 +9,8 @@ namespace TestMongoDbImport
     public class ConvertTests
     {
         //[TestCase("delicious", @"C:\code\csharp6\Tagging-Util\storage\delicious-2016-03-16.html", @"C:\code\csharp6\Tagging-Util\storage\delicious-2016-03-16.json")]
-        [TestCase("gitmarks", @"C:\code\csharp6\Tagging-Util\storage\gitmarks", @"C:\code\csharp6\Tagging-Util\storage\gitmarks.json")]
+        //[TestCase("gitmarks", @"C:\code\csharp6\Tagging-Util\storage\gitmarks", @"C:\code\csharp6\Tagging-Util\storage\gitmarks.json")]
+        //[TestCase("pinterest", @"C:\code\csharp6\Tagging-Util\storage\pins.txt", @"C:\code\csharp6\Tagging-Util\storage\pinterest-nov21-2016.json")]
         public void TestConvert(string parserType, string bookmarksFile, string outputPath)
         {
             Program.ParseBookmarks(parserType, bookmarksFile, outputPath);
--- TestMongoDbImport/MergeTests.cs
+++ TestMongoDbImport/MergeTests.cs
@@ -9,9 +9,15 @@ namespace TestMongoDbImport
     [TestFixture]
     public class MergeTests
     {
-        [TestCase(@"C:\code\csharp6\Tagging-Util\storage\delicious-2016-03-16.json"
-                , @"C:\code\csharp6\Tagging-Util\storage\gitmarks-apr20-2016.json"
-                , @"C:\code\csharp6\Tagging-Util\storage\bookmarks.json")]
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\delicious-2016-03-16.json"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\gitmarks-apr20-2016.json"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge.json")]
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\backup-nov21.txt"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\gitmarks-nov21-2016.json"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge.json")]
+        [TestCase(@"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-nov21-2016.json"
+                , @"C:\code\csharp6\Tagging-Util\storage\pinterest-nov21-2016.json"
+                , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge.json")]
         public void TestMergeBookmarks(string file1, string file2, string outputPath)
         {
             var bookmarks = MergeBookmarks.Merge(file1, file2);
