3ed0feb069df4704b5bfb1600db2a050df48952f
usametov <usametov@gmail.com>
2017-02-04 16:22:05 +0600

    test merge to mongo format

--- TestMongoDbImport/MergeTests.cs
+++ TestMongoDbImport/MergeTests.cs
@@ -18,9 +18,9 @@ namespace TestMongoDbImport
         //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-nov21-2016.json"
         //        , @"C:\code\csharp6\Tagging-Util\storage\pinterest-nov21-2016.json"
         //        , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge.json")]
-        [TestCase(@"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-nov22-2016.json"
-                , @"C:\code\csharp6\Tagging-Util\storage\gitmarks-jan25.json"
-                , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-feb2.json")]
+        //[TestCase(@"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-nov22-2016.json"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\gitmarks-jan25.json"
+        //        , @"C:\code\csharp6\Tagging-Util\storage\bookmarks-merge-feb2.json")]
         public void TestMergeBookmarks(string file1, string file2, string outputPath)
         {
             var bookmarks = MergeBookmarks.Merge(file1, file2);
