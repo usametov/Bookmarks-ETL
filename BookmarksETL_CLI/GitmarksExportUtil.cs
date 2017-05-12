a1dda7c903e4f1359dab2dbe6aeb5f03693aa5a0
usametov <usametov@gmail.com>
2017-05-12 15:43:11 -0400

    fix bug with empty url

--- BookmarksETL_CLI/GitmarksExportUtil.cs
+++ BookmarksETL_CLI/GitmarksExportUtil.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Newtonsoft.Json;
 using System;
 using GitmarksParser;
+using System.Security.Cryptography;
 
 namespace BookmarksETL_CLI
 {
@@ -19,27 +20,34 @@ namespace BookmarksETL_CLI
         {
             var bookmarks = JsonConvert.DeserializeObject<Bookmark[]>(File.ReadAllText(bookmarksFile)).AsEnumerable();
 
-            var result = Input.From(bookmarks).Apply(row => {
+            using (var md5 = MD5.Create())
+            {
+                var result = Input.From(bookmarks).Apply(row => {
 
-                                                        string fileName = Path.Combine(bookmarksDir, row["Id"].ToString());
+                    if (string.IsNullOrEmpty(row["LinkUrl"].ToString()))
+                        return;
 
-                                                        var bookmark = new BookmarkContent
-                                                        {
-                                                            hash = row["Id"].ToString()
-                                                            ,
-                                                            creator = "batch"
-                                                            ,
-                                                            rights = "CC BY"
-                                                            ,
-                                                            time = DateTime.Now.ToShortDateString()
-                                                            ,
-                                                            title = row["LinkText"].ToString()
-                                                            ,
-                                                            uri = row["LinkUrl"].ToString()
-                                                        };
-                                                        //dump bookmark
-                                                        File.WriteAllText(fileName, JsonConvert.SerializeObject(bookmark)); 
-                                                    }).Execute();
+                    var id = Utils.ComputeHash(row["LinkUrl"].ToString(), md5);
+                    string fileName = Path.Combine(bookmarksDir, id);
+
+                    var bookmark = new BookmarkContent
+                    {
+                        hash = id
+                        ,
+                        creator = "batch"
+                        ,
+                        rights = "CC BY"
+                        ,
+                        time = DateTime.Now.ToShortDateString()
+                        ,
+                        title = row["LinkText"].ToString()
+                        ,
+                        uri = row["LinkUrl"].ToString()
+                    };
+                    //dump bookmark
+                    File.WriteAllText(fileName, JsonConvert.SerializeObject(bookmark));
+                }).Execute();
+            }
         }
         /// <summary>
         /// exports tags in gitmarks format
@@ -49,50 +57,55 @@ namespace BookmarksETL_CLI
         public static void ExportTags(string bookmarksFile, string tagsDir)
         {
             var bookmarks = JsonConvert.DeserializeObject<Bookmark[]>(File.ReadAllText(bookmarksFile)).AsEnumerable();
-            
-            var invertedTags = bookmarks.SelectMany
-                (b => {//invert tags
-                        return b.Tags.Select(t => 
-                                            {
+
+            using (var md5 = MD5.Create())
+            {
+                var invertedTags = bookmarks.SelectMany
+                    (b =>
+                    {//invert tags
+                        return b.Tags.Select(t =>
+                                        {
                                                 var invalidFileNameChars = Path.GetInvalidFileNameChars()
-                                                                               .Concat(new[] { '[', ']', '#', '!' });
+                                                                                   .Concat(new[] { '[', ']', '#', '!' });
                                                 //tag will become file name, so clean it up 
-                                                t = invalidFileNameChars.Aggregate(t, (current, c) => 
-                                                                                       current.Replace(c.ToString(), string.Empty));
+                                                t = invalidFileNameChars.Aggregate(t, (current, c) =>
+                                                                                           current.Replace(c.ToString(), string.Empty));
                                                 //map stage
                                                 return Tuple.Create(t, new TagContent
-                                                                    {
-                                                                        creator = "batch"
-                                                                    ,
-                                                                        hash = b.Id
-                                                                    ,
-                                                                        title = b.LinkText
-                                                                    ,
-                                                                        uri = b.LinkUrl
-                                                                    ,
-                                                                        ver = "BookmarksEtl.0.0.1"
-                                                                    });
-                                            });
+                                                    {
+                                                        creator = "batch"
+                                                                        ,
+                                                        hash = string.IsNullOrEmpty(b.Id) ? Utils.ComputeHash(b.LinkUrl, md5) : b.Id
+                                                                        ,
+                                                        title = b.LinkText
+                                                                        ,
+                                                        uri = b.LinkUrl
+                                                                        ,
+                                                        ver = "BookmarksEtl.0.0.1"
+                                                    });
+                                                });
                     })
-                    .GroupBy(it => it.Item1)
-                    .OrderBy(it=>it.Key);//group by tag and sort
-            
-            var result = Input.From(invertedTags.ToList()).Apply(row => 
-            {
-                var tagName = row["Key"].ToString().Trim();                                
+                        .GroupBy(it => it.Item1)
+                        .OrderBy(it => it.Key);//group by tag and sort
 
-                if (string.IsNullOrEmpty(tagName)) return;
+                var result = Input.From(invertedTags.ToList()).Apply(row =>
+                {
+                    var tagName = row["Key"].ToString().Trim();
 
-                string fileName = Path.Combine(tagsDir, tagName);
+                    if (string.IsNullOrEmpty(tagName)) return;
+
+                    string fileName = Path.Combine(tagsDir, tagName);
+
+                    var tagContentTuples = row["elements"] as Tuple<string, TagContent>[];
                 
-                var tagContentTuples = row["elements"] as Tuple<string, TagContent>[];
-                //reduce stage
-                var tagContent = tagContentTuples.Where(tpl=>tpl!=null).Select(tpl => tpl.Item2);
+                    //reduce stage
+                    var tagContent = tagContentTuples.Where(tpl => tpl != null).Select(tpl => tpl.Item2);
 
-                var content = JsonConvert.SerializeObject(tagContent.ToArray());
+                    var content = JsonConvert.SerializeObject(tagContent.ToArray());
 
-                File.WriteAllText(fileName, content);
-            }).Execute();
+                    File.WriteAllText(fileName, content);
+                }).Execute();
+            }
         }
     }
 }
